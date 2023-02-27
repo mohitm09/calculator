6204cdd85aefd17ad840de57d4902d463dc7d630
Shivam <shivamtirath@gmail.com>
2023-02-27 14:22:06 +0530

    display number

--- script.js
+++ script.js
@@ -14,4 +14,25 @@ class Calculator {
     delete() {
         this.currentOperand = this.currentOperand.toString().slice(0, -1)
     }
+
+    /* add additional code here
+    till compute
+    */
+
+    getDisplayNumber(number) {
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+            integerDisplay = ''
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
+        }
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay
+        }
+    }
 }
